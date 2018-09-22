647121add33d9f4d425706b1a998b22290cfa583
dshiosaky <dshiosaky@dshiosaky-LTP>
2018-09-22 17:26:09 -0400

    xx

--- Test Suites/Login TestSuite.ts
+++ Test Suites/Login TestSuite.ts
@@ -4,10 +4,29 @@
    <name>Login TestSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-09-22T17:17:57</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>73b592ad-5182-432e-9256-cb4f83405afe</testSuiteGuid>
+   <testCaseLink>
+      <guid>898fea6c-7ced-4e04-825f-a889486203cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Succefull Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d5409cb4-f4c8-412f-9348-090d11730310</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Failed Login 1 attempt</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ac39b67-a009-4d86-b756-0c6cb63f168d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Failed log in 3 Attempts</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
